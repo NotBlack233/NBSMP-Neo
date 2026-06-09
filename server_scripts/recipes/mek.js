baae8762dbb58acbfcfc1a9193c4c32097c20d1d
Not_Black <not_black_233@163.com>
2026-06-09 12:56:07 +0800

    Add attachment recipes

--- server_scripts/recipes/mek.js
+++ server_scripts/recipes/mek.js
@@ -20,9 +20,115 @@ ServerEvents.recipes(event => {
         'create_new_age:layered_magnet',
         MekanismChemical.stack('mekanism:diamond', 40)
     )
+    event.custom({
+        "type": "mekanism:metallurgic_infusing",
+        "chemical_input": { "amount": 320, "tag": "mekanism:diamond" },
+        "item_input": {
+            "type": "neoforge:components",
+            "components": {
+                "minecraft:custom_data": { AttachmentId: "tacz:extended_mag_1" }
+            },
+            "items": "tacz:attachment"
+        },
+        "output": Item.of('tacz:attachment', { custom_data: { AttachmentId: "tacz:extended_mag_2" } }).toJson(),
+        "per_tick_usage": false
+    })
+    event.custom({
+        "type": "mekanism:metallurgic_infusing",
+        "chemical_input": { "amount": 320, "tag": "mekanism:diamond" },
+        "item_input": {
+            "type": "neoforge:components",
+            "components": {
+                "minecraft:custom_data": { AttachmentId: "tacz:light_extended_mag_1" }
+            },
+            "items": "tacz:attachment"
+        },
+        "output": Item.of('tacz:attachment', { custom_data: { AttachmentId: "tacz:light_extended_mag_2" } }).toJson(),
+        "per_tick_usage": false
+    })
+    event.custom({
+        "type": "mekanism:metallurgic_infusing",
+        "chemical_input": { "amount": 320, "tag": "mekanism:diamond" },
+        "item_input": {
+            "type": "neoforge:components",
+            "components": {
+                "minecraft:custom_data": { AttachmentId: "tacz:shotgun_extended_mag_1" }
+            },
+            "items": "tacz:attachment"
+        },
+        "output": Item.of('tacz:attachment', { custom_data: { AttachmentId: "tacz:shotgun_extended_mag_2" } }).toJson(),
+        "per_tick_usage": false
+    })
+    event.custom({
+        "type": "mekanism:metallurgic_infusing",
+        "chemical_input": { "amount": 320, "tag": "mekanism:diamond" },
+        "item_input": {
+            "type": "neoforge:components",
+            "components": {
+                "minecraft:custom_data": { AttachmentId: "tacz:sniper_extended_mag_1" }
+            },
+            "items": "tacz:attachment"
+        },
+        "output": Item.of('tacz:attachment', { custom_data: { AttachmentId: "tacz:sniper_extended_mag_2" } }).toJson(),
+        "per_tick_usage": false
+    })
+    event.custom({
+        "type": "mekanism:metallurgic_infusing",
+        "chemical_input": { "amount": 640, "tag": "mekanism:refined_obsidian" },
+        "item_input": {
+            "type": "neoforge:components",
+            "components": {
+                "minecraft:custom_data": { AttachmentId: "tacz:extended_mag_2" }
+            },
+            "items": "tacz:attachment"
+        },
+        "output": Item.of('tacz:attachment', { custom_data: { AttachmentId: "tacz:extended_mag_3" } }).toJson(),
+        "per_tick_usage": false
+    })
+    event.custom({
+        "type": "mekanism:metallurgic_infusing",
+        "chemical_input": { "amount": 640, "tag": "mekanism:refined_obsidian" },
+        "item_input": {
+            "type": "neoforge:components",
+            "components": {
+                "minecraft:custom_data": { AttachmentId: "tacz:light_extended_mag_2" }
+            },
+            "items": "tacz:attachment"
+        },
+        "output": Item.of('tacz:attachment', { custom_data: { AttachmentId: "tacz:light_extended_mag_3" } }).toJson(),
+        "per_tick_usage": false
+    })
+    event.custom({
+        "type": "mekanism:metallurgic_infusing",
+        "chemical_input": { "amount": 640, "tag": "mekanism:refined_obsidian" },
+        "item_input": {
+            "type": "neoforge:components",
+            "components": {
+                "minecraft:custom_data": { AttachmentId: "tacz:shotgun_extended_mag_2" }
+            },
+            "items": "tacz:attachment"
+        },
+        "output": Item.of('tacz:attachment', { custom_data: { AttachmentId: "tacz:shotgun_extended_mag_3" } }).toJson(),
+        "per_tick_usage": false
+    })
+    event.custom({
+        "type": "mekanism:metallurgic_infusing",
+        "chemical_input": { "amount": 640, "tag": "mekanism:refined_obsidian" },
+        "item_input": {
+            "type": "neoforge:components",
+            "components": {
+                "minecraft:custom_data": { AttachmentId: "tacz:sniper_extended_mag_2" }
+            },
+            "items": "tacz:attachment"
+        },
+        "output": Item.of('tacz:attachment', { custom_data: { AttachmentId: "tacz:sniper_extended_mag_3" } }).toJson(),
+        "per_tick_usage": false
+    })
+    
     mek.combining(
         'create_new_age:netherite_magnet',
         'create_new_age:fluxuated_magnetite',
         '2x minecraft:netherite_scrap'
     )
+
 })
--- server_scripts/recipes/tacz.js
+++ server_scripts/recipes/tacz.js
@@ -115,6 +115,82 @@ ServerEvents.recipes(event => {
         {item: {tag: 'c:plates/iron'}, count: 4},
         {item: {tag: 'c:glass_blocks'}, count: 4}
     ])
+    gunsmith({type: 'attachment', id: 'tacz:extended_mag_1'}, [
+        {item: {item: 'immersiveengineering:component_iron'}, count: 12},
+        {item: {tag: 'c:plates/gold'}, count: 4}
+    ])
+    gunsmith({type: 'attachment', id: 'tacz:light_extended_mag_1'}, [
+        {item: {item: 'immersiveengineering:component_iron'}, count: 8},
+        {item: {tag: 'c:plates/gold'}, count: 1}
+    ])
+    gunsmith({type: 'attachment', id: 'tacz:shotgun_extended_mag_1'}, [
+        {item: {item: 'immersiveengineering:component_iron'}, count: 10},
+        {item: {tag: 'c:plates/gold'}, count: 4}
+    ])
+    gunsmith({type: 'attachment', id: 'tacz:sniper_extended_mag_1'}, [
+        {item: {item: 'immersiveengineering:component_iron'}, count: 16},
+        {item: {tag: 'c:plates/gold'}, count: 6}
+    ])
+    gunsmith({type: 'attachment', id: 'tacz:ammo_mod_i'}, [
+        {item: {
+            "type": "neoforge:components",
+            "components": {
+                "minecraft:custom_data": { AttachmentId: "tacz:extended_mag_3" }
+            },
+            "items": "tacz:attachment"
+        }},
+        {item: {item: 'minecraft:fire_charge'}, count: 16},
+        {item: {item: 'minecraft:blaze_powder'}, count: 16},
+        {item: {item: 'minecraft:crying_obsidian'}, count: 2}
+    ])
+    gunsmith({type: 'attachment', id: 'tacz:ammo_mod_hp'}, [
+        {item: {
+            "type": "neoforge:components",
+            "components": {
+                "minecraft:custom_data": { AttachmentId: "tacz:extended_mag_3" }
+            },
+            "items": "tacz:attachment"
+        }},
+        {item: {item: 'minecraft:crying_obsidian'}, count: 3},
+        {item: {tag: 'c:ingots/steel'}, count: 8}
+    ])
+    gunsmith({type: 'attachment', id: 'tacz:ammo_mod_he'}, [
+        {item: {
+            "type": "neoforge:components",
+            "components": {
+                "minecraft:custom_data": { AttachmentId: "tacz:ammo_mod_slug" }
+            },
+            "items": "tacz:attachment"
+        }},
+        {item: {item: 'minecraft:end_crystal'}, count: 4},
+        {item: {item: 'minecraft:blaze_powder'}, count: 8},
+        {item: {item: 'minecraft:crying_obsidian'}, count: 2},
+        {item: {item: 'mekanismadditions:obsidian_tnt'}, count: 16}
+    ])
+    gunsmith({type: 'attachment', id: 'tacz:ammo_mod_slug'}, [
+        {item: {
+            "type": "neoforge:components",
+            "components": {
+                "minecraft:custom_data": { AttachmentId: "tacz:extended_mag_3" }
+            },
+            "items": "tacz:attachment"
+        }},
+        {item: {item: 'minecraft:crying_obsidian'}, count: 3},
+        {item: {item: 'minecraft:netherite_scrap'}},
+        {item: {tag: 'c:storage_blocks/osmium'}}
+    ])
+    gunsmith({type: 'attachment', id: 'tacz:ammo_mod_fmj'}, [
+        {item: {
+            "type": "neoforge:components",
+            "components": {
+                "minecraft:custom_data": { AttachmentId: "tacz:extended_mag_3" }
+            },
+            "items": "tacz:attachment"
+        }},
+        {item: {item: 'minecraft:crying_obsidian'}, count: 2},
+        {item: {item: 'minecraft:diamond'}, count: 5},
+        {item: {tag: 'c:ingots/steel'}, count: 16}
+    ])
 
     // Early game
     gunsmith({type: 'gun', 'id': 'tacz:m1911'}, [
@@ -149,6 +225,46 @@ ServerEvents.recipes(event => {
         {item: {item: 'create:andesite_alloy'}, count: 6}
     ])
     // Middle game
+    gunsmith({type: 'gun', id: 'tacz:glock_17'}, [
+        {item: {item: 'create:precision_mechanism'}},
+        {item: {item: 'immersiveengineering:component_iron'}, count: 8},
+        {item: {item: 'immersiveengineering:component_steel'}, count: 2}
+    ])
+    gunsmith({type: 'gun', id: 'tacz:cz75'}, [
+        {item: {item: 'create:precision_mechanism'}},
+        {item: {item: 'immersiveengineering:component_iron'}, count: 6},
+        {item: {item: 'immersiveengineering:component_steel'}, count: 4}
+    ])
+    gunsmith({type: 'gun', id: 'tacz:m9a4'}, [
+        {item: {item: 'create:precision_mechanism'}},
+        {item: {item: 'immersiveengineering:component_iron'}, count: 8},
+        {item: {item: 'immersiveengineering:component_steel'}, count: 2}
+    ])
+    gunsmith({type: 'gun', id: 'tacz:p320'}, [
+        {item: {item: 'create:precision_mechanism'}},
+        {item: {item: 'immersiveengineering:component_iron'}, count: 10},
+        {item: {item: 'immersiveengineering:component_steel'}, count: 6},
+        {item: {tag: 'c:plates/obsidian'}, count: 4}
+    ])
+    gunsmith({type: 'gun', id: 'tacz:b93r'}, [
+        {item: {item: 'create:precision_mechanism'}},
+        {item: {tag: 'immersiveengineering:treated_wood'}, count: 8},
+        {item: {item: 'immersiveengineering:component_iron'}, count: 10},
+        {item: {item: 'immersiveengineering:component_steel'}, count: 4},
+    ])
+    gunsmith({type: 'gun', id: 'tacz:rhino357'}, [
+        {item: {item: 'create:precision_mechanism'}},
+        {item: {tag: 'immersiveengineering:treated_wood'}, count: 8},
+        {item: {item: 'immersiveengineering:component_iron'}, count: 12},
+        {item: {item: 'immersiveengineering:component_steel'}, count: 6},
+        {item: {tag: 'c:plates/obsidian'}, count: 6}
+    ])
+    gunsmith({type: 'gun', id: 'tacz:hk_mk23'}, [
+        {item: {item: 'create:precision_mechanism'}},
+        {item: {item: 'immersiveengineering:component_iron'}, count: 12},
+        {item: {item: 'immersiveengineering:component_steel'}, count: 6},
+        {item: {tag: 'c:plates/obsidian'}, count: 6}
+    ])
     gunsmith({type: 'gun', id: 'tacz:kar98'}, [
         {item: {tag: 'minecraft:logs'}, count: 40},
         {item: {item: 'immersiveengineering:component_iron'}, count: 18},
@@ -156,6 +272,131 @@ ServerEvents.recipes(event => {
         {item: {tag: 'c:plates/obsidian'}, count: 16},
         {item: {tag: 'c:plates/brass'}, count: 6},
         {item: {item: 'create:precision_mechanism'}}
+    ])
+    gunsmith({type: 'gun', id: 'tacz:m700', attachments: {"scope": "tacz:scope_contender"}}, [
+        {item: {item: 'create:precision_mechanism'}},
+        {item: {item: 'immersiveengineering:component_iron'}, count: 16},
+        {item: {item: 'immersiveengineering:component_steel'}, count: 16},
+        {item: {item: 'mekanism:alloy_reinforced'}, count: 4}
+    ])
+    gunsmith({type: 'gun', id: 'tacz:ak47'}, [
+        {item: {item: 'create:precision_mechanism'}},
+        {item: {tag: 'immersiveengineering:treated_wood'}, count: 16},
+        {item: {item: 'immersiveengineering:component_iron'}, count: 16},
+        {item: {item: 'immersiveengineering:component_steel'}, count: 6}
+    ])
+    gunsmith({type: 'gun', id: 'tacz:m4a1', attachments: {"stock": "tacz:stock_m4ss"}}, [
+        {item: {item: 'create:precision_mechanism'}},
+        {item: {tag: 'immersiveengineering:treated_wood'}, count: 8},
+        {item: {item: 'immersiveengineering:component_iron'}, count: 14},
+        {item: {item: 'immersiveengineering:component_steel'}, count: 4}
+    ])
+    gunsmith({type: 'gun', id: 'tacz:scar_l'}, [
+        {item: {item: 'create:precision_mechanism'}},
+        {item: {item: 'immersiveengineering:component_iron'}, count: 18},
+        {item: {item: 'immersiveengineering:component_steel'}, count: 4}
+    ])
+    gunsmith({type: 'gun', id: 'tacz:m16a1'}, [
+        {item: {item: 'create:precision_mechanism'}},
+        {item: {tag: 'immersiveengineering:treated_wood'}, count: 8},
+        {item: {item: 'immersiveengineering:component_iron'}, count: 18},
+        {item: {item: 'immersiveengineering:component_steel'}, count: 6}
+    ])
+    gunsmith({type: 'gun', id: 'tacz:m16a4'}, [
+        {item: {item: 'create:precision_mechanism'}},
+        {item: {tag: 'immersiveengineering:treated_wood'}, count: 8},
+        {item: {item: 'immersiveengineering:component_iron'}, count: 18},
+        {item: {item: 'immersiveengineering:component_steel'}, count: 6}
+    ])
+    gunsmith({type: 'gun', id: 'tacz:hk416d', attachments: {"stock": "tacz:stock_hk_slim_line"}}, [
+        {item: {item: 'create:precision_mechanism'}},
+        {item: {item: 'immersiveengineering:component_iron'}, count: 30},
+        {item: {item: 'immersiveengineering:component_steel'}, count: 10},
+        {item: {item: 'mekanism:alloy_reinforced'}, count: 8}
+    ])
+    gunsmith({type: 'gun', id: 'tacz:aug'}, [
+        {item: {item: 'create:precision_mechanism'}},
+        {item: {tag: 'immersiveengineering:treated_wood'}, count: 16},
+        {item: {item: 'immersiveengineering:component_iron'}, count: 26},
+        {item: {item: 'immersiveengineering:component_steel'}, count: 10},
+        {item: {item: 'mekanism:alloy_reinforced'}, count: 4},
+        {item: {tag: 'c:gems/amethyst'}}
+    ])
+    gunsmith({type: 'gun', id: 'tacz:type_81'}, [
+        {item: {item: 'create:precision_mechanism'}},
+        {item: {tag: 'immersiveengineering:treated_wood'}, count: 16},
+        {item: {item: 'immersiveengineering:component_iron'}, count: 16},
+        {item: {item: 'immersiveengineering:component_steel'}, count: 6}
+    ])
+    gunsmith({type: 'gun', id: 'tacz:qbz_95'}, [
+        {item: {item: 'create:precision_mechanism'}},
+        {item: {tag: 'immersiveengineering:treated_wood'}, count: 16},
+        {item: {item: 'immersiveengineering:component_iron'}, count: 14},
+        {item: {item: 'immersiveengineering:component_steel'}, count: 8},
+        {item: {item: 'mekanism:alloy_reinforced'}, count: 4}
+    ])
+    gunsmith({type: 'gun', id: 'tacz:spr15hb'}, [
+        {item: {item: 'create:precision_mechanism'}},
+        {item: {item: 'immersiveengineering:component_iron'}, count: 32},
+        {item: {item: 'immersiveengineering:component_steel'}, count: 10},
+        {item: {item: 'mekanism:alloy_reinforced'}, count: 8},
+        {item: {tag: 'c:plates/obsidian'}, count: 8},
+        {item: {tag: 'c:dusts/hop_graphite'}}
+    ])
+    gunsmith({type: 'gun', id: 'tacz:qbz_191'}, [
+        {item: {item: 'create:precision_mechanism'}},
+        {item: {tag: 'immersiveengineering:treated_wood'}, count: 16},
+        {item: {item: 'immersiveengineering:component_iron'}, count: 14},
+        {item: {item: 'immersiveengineering:component_steel'}, count: 8},
+        {item: {item: 'mekanism:alloy_reinforced'}, count: 4}
+    ])
+    gunsmith({type: 'gun', id: 'tacz:m870'}, [
+        {item: {item: 'create:precision_mechanism'}},
+        {item: {tag: 'immersiveengineering:treated_wood'}, count: 16},
+        {item: {item: 'immersiveengineering:component_iron'}, count: 12},
+        {item: {item: 'immersiveengineering:component_steel'}, count: 4}
+    ])
+    gunsmith({type: 'gun', id: 'tacz:m1014'}, [
+        {item: {item: 'create:precision_mechanism'}},
+        {item: {item: 'immersiveengineering:component_iron'}, count: 32},
+        {item: {item: 'immersiveengineering:component_steel'}, count: 12},
+        {item: {item: 'mekanism:alloy_reinforced'}, count: 6}
+    ])
+    gunsmith({type: 'gun', id: 'tacz:hk_mp5a5'}, [
+        {item: {item: 'create:precision_mechanism'}},
+        {item: {item: 'immersiveengineering:component_iron'}, count: 10},
+        {item: {item: 'immersiveengineering:component_steel'}, count: 4}
+    ])
+    gunsmith({type: 'gun', id: 'tacz:vector45'}, [
+        {item: {item: 'create:precision_mechanism'}},
+        {item: {item: 'immersiveengineering:component_iron'}, count: 24},
+        {item: {item: 'immersiveengineering:component_steel'}, count: 8},
+        {item: {item: 'mekanism:alloy_reinforced'}, count: 10}
+    ])
+    gunsmith({type: 'gun', id: 'tacz:ump45'}, [
+        {item: {item: 'create:precision_mechanism'}},
+        {item: {item: 'immersiveengineering:component_iron'}, count: 28},
+        {item: {item: 'immersiveengineering:component_steel'}, count: 10},
+        {item: {item: 'mekanism:alloy_reinforced'}, count: 12}
+    ])
+    gunsmith({type: 'gun', id: 'tacz:rpk', attachments: {"stock": "tacz:oem_stock_heavy"}}, [
+        {item: {item: 'create:precision_mechanism'}},
+        {item: {tag: 'immersiveengineering:treated_wood'}, count: 32},
+        {item: {item: 'immersiveengineering:component_iron'}, count: 24},
+        {item: {item: 'immersiveengineering:component_steel'}, count: 10},
+        {item: {item: 'mekanism:alloy_reinforced'}, count: 8}
+    ])
+    gunsmith({type: 'gun', id: 'tacz:rpg7'}, [
+        {item: {item: 'create:precision_mechanism'}},
+        {item: {tag: 'immersiveengineering:treated_wood'}, count: 32},
+        {item: {item: 'immersiveengineering:component_iron'}, count: 8},
+        {item: {item: 'immersiveengineering:component_steel'}, count: 16}
+    ])
+    gunsmith({type: 'gun', id: 'tacz:m320'}, [
+        {item: {item: 'create:precision_mechanism'}},
+        {item: {item: 'immersiveengineering:component_iron'}, count: 14},
+        {item: {item: 'immersiveengineering:component_steel'}, count: 24},
+        {item: {item: 'mekanism:alloy_reinforced'}, count: 4}
     ])
     // Late game
 })
